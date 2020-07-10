161f2731a6d947b604ef5cd6605801dbac6609a4
Roman-js <liverkuzen1989@gmail.com>
2020-07-10 18:25:46 +0300

    Updates

---
+++ precache-manifest.c6af541664c0cad3cdd759387da6a59d.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2b68d6a8a46c1677ba72e11de5d7770b",
+    "url": "/portfolio/index.html"
+  },
+  {
+    "revision": "55fc6ef130caa3ca6837",
+    "url": "/portfolio/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "d3a51ed187374089e7d0",
+    "url": "/portfolio/static/js/2.9e384841.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/portfolio/static/js/2.9e384841.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "55fc6ef130caa3ca6837",
+    "url": "/portfolio/static/js/main.80b18416.chunk.js"
+  },
+  {
+    "revision": "f619aeccc94a0d089131",
+    "url": "/portfolio/static/js/runtime-main.075c0788.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/portfolio/static/media/logo.5d5d9eef.svg"
+  }
+]);
