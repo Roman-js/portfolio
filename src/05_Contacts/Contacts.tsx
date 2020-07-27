e208911a31f1360347bf317b58ee1f097f0c55d3
Roman-js <liverkuzen1989@gmail.com>
2020-07-27 15:16:14 +0300

    Connect form to heroku

--- src/05_Contacts/Contacts.tsx
+++ src/05_Contacts/Contacts.tsx
@@ -1,11 +1,16 @@
-import React from "react";
+import React, {useState} from "react";
 import style from './Contacts.module.scss'
 import {Button, Container, TextField} from "@material-ui/core";
 import Title from "../07_Common/Title/Title";
-
 import PropTypes from "prop-types";
-import { withStyles } from "@material-ui/core/styles";
+import {withStyles} from "@material-ui/core/styles";
+import axios from 'axios'
+
 const Fade: any = require('react-reveal').default;
+
+ const instance = axios.create({
+     baseURL: 'https://gmail-smtp.herokuapp.com/sendMessage'}
+ );
 
 
 
@@ -18,40 +23,67 @@ const styles = {
     },
 
 };
+type initialStateType = {
+    name: string,
+    email: string,
+    message: string
+}
 
 const Contacts = (props: any) => {
 
-    const { classes } = props;
+    const {classes} = props;
+
+    const [state, setState] = useState<initialStateType>({name: '', email: '', message: ''});
+
+    const setName = (e: any) => {
+        setState({...state, name: e.currentTarget.value});
+        console.log(state)
+    };
+    const setEmail = (e: any) => {
+        setState({...state, email: e.currentTarget.value});
+    };
+    const setMessage = (e: any) => {
+        setState({...state, message: e.currentTarget.value});
+    };
+    const sendForm = () =>{
+      return axios.post('https://gmail-smtp.herokuapp.com/sendMessage', state)
+    };
 
 
-
-
-        return (
+    return (
         <div className={style.contacts} id={'ToContacts'}>
 
             <Container maxWidth={'md'} className={style.container}>
                 <Fade>
-                <Title title='Contacts'/>
-                <div className={style.insideContainer}>
-                    <form className={style.form}>
-                        <TextField placeholder='Name'
-                                   className={classes.root}
-                                   InputProps={{className: classes.input}}
-                        />
-                        <TextField placeholder='Email'
-                                   className={classes.root}
-                                   InputProps={{className: classes.input}}
-                        />
-                        <TextField placeholder='Your message'
-                                   className={classes.root}
-                                   InputProps={{className: classes.input}}
-                        />
-                        <div className={style.button}>
-                            <Button variant='outlined' color='primary'>Send</Button>
-                        </div>
+                    <Title title='Contacts'/>
+                    <div className={style.insideContainer}>
+                        <form className={style.form}>
+                            <TextField placeholder='Name'
+                                       onChange={setName}
+                                value={state.name}
+                                       className={classes.root}
+                                       InputProps={{className: classes.input}}
+                            />
+                            <TextField placeholder='Email'
+                                       onChange={setEmail}
+                                       value={state.email}
+                                       className={classes.root}
+                                       InputProps={{className: classes.input}}
+                            />
+                            <TextField placeholder='Your message'
+                                       onChange={setMessage}
+                                       value={state.message}
+                                       className={classes.root}
+                                       InputProps={{className: classes.input}}
+                            />
+                            <div className={style.button}>
+                                <Button variant='outlined'
+                                        onClick={sendForm}
+                                        color='primary'>Send</Button>
+                            </div>
 
-                    </form>
-                </div>
+                        </form>
+                    </div>
                 </Fade>
             </Container>
 
